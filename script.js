93588cb56e5f6098216ca16e3dfb989dc8375e97
Jayant Ghadge <jayantghadge6@gmail.com>
2023-10-28 11:06:52 +0530

    Improve CSS styles, including transitions

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuIcon = document.querySelector(".menu-icon");
-const lines = document.querySelector(".lines");
 const container = document.querySelector(".container");
+const header = document.querySelector(".header");
+const hElements = document.getElementsByTagName("h1");
 
 menuIcon.addEventListener("click", () => {
   container.classList.toggle("change");
+  header.classList.toggle("shift-header");
+
+  for (let i = 0; i < hElements.length; i++) {
+    hElements[i].classList.toggle("change-h1");
+  }
 });
